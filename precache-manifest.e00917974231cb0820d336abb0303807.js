97bcba1c3f77e6d16814fcc9441a6e41df1db145
IareDave <gouletdavid95@gmail.com>
2019-06-02 19:21:13 -0400

    Updates

---
+++ precache-manifest.e00917974231cb0820d336abb0303807.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c50e489df4a75c2a210d5760fa95a438",
+    "url": "/notes-app-client/index.html"
+  },
+  {
+    "revision": "828f2fe05483689f9b7a",
+    "url": "/notes-app-client/static/css/main.bea631d4.chunk.css"
+  },
+  {
+    "revision": "82a68bd2daa3eb3fb314",
+    "url": "/notes-app-client/static/js/2.0b6344b0.chunk.js"
+  },
+  {
+    "revision": "828f2fe05483689f9b7a",
+    "url": "/notes-app-client/static/js/main.3c7ff931.chunk.js"
+  },
+  {
+    "revision": "3ccd64a772ffdfaf0eab",
+    "url": "/notes-app-client/static/js/runtime~main.46c92b78.js"
+  }
+]);
